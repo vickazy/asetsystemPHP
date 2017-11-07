4f2f4f2512fd9b5720122f09d84d1022e66fc83e
herry88 <herry081288@gmail.com>
2017-11-07 09:30:16 +0700

    NewCode

--- modul/mod_mutasi/mutasi.php
+++ modul/mod_mutasi/mutasi.php
@@ -20,7 +20,7 @@ else {
    switch($act){ 
         default:
             	//Tampil data mutasi
-			echo "<div>
+		echo "<div>
             <ul class=\"breadcrumb\">
                 <li>
                     <a href=\"?module=beranda\">Home</a>
@@ -31,6 +31,40 @@ else {
             </ul>
             </div>";
 
+            echo "<div class=\"row\">
+            <div class=\"box col-md-12\">
+            <div class=\"box-inner\">
+            <div class=\"box-header well\" data-original-title=\"\">
+                <h2><i class=\"glyphicon glyphicon-user\"></i> Data Mutasi Aset</h2>
+        
+                <div class=\"box-icon\">
+                    <a href=\"#\" class=\"btn btn-setting btn-round btn-default\"><i class=\"glyphicon glyphicon-cog\"></i></a>
+                    <a href=\"#\" class=\"btn btn-minimize btn-round btn-default\"><i
+                            class=\"glyphicon glyphicon-chevron-up\"></i></a>
+                    <a href=\"#\" class=\"btn btn-close btn-round btn-default\"><i class=\"glyphicon glyphicon-remove\"></i></a>
+                </div>
+            </div>
+            <div class=\"box-content\">
+            <table class=\"table table-striped table-bordered bootstrap-datatable datatable responsive\">
+            <thead>
+            <tr>
+                <th>No Aset</th>
+                <th>Nama Aset</th>
+                <th>PIC</th>
+                <th>Noreg</th>
+                <th>Area</th>
+                <th>Customer</th>
+                <th>Cluster</th>
+                <th>Tgl. Pindah</th>
+                <th>Keterangan</th>
+                <th>Status</th>
+            </tr>
+            </thead>";
+           echo"</table>
+                </div>
+                </div>
+                </div>
+            </div>";
 
         break;
    }     
