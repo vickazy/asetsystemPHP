b30b66d7816951ed91c858f5f07b0d499354e3ce
herry88 <herry081288@gmail.com>
2017-11-07 08:53:51 +0700

    mutasiPHP

--- modul/mod_mutasi/mutasi.php
+++ modul/mod_mutasi/mutasi.php
@@ -1,4 +1,18 @@
 <?php 
 if(empty($_SESSION['namauser']) AND empty($_SESSION['passuser'])){
-    
+    echo "<link href=\"../css/style_login.css\" type=\"text/css\" rel=\"stylesheet\">
+    <p class=\"error-code\"> 404</p>
+        <p class=\"not-found\">Not<br/>Found</p>
+        <div class=\"clear\"></div>
+        <div class=\"content\">
+            Silahkan untuk login terlebih dahulu.
+            <br>
+            <a href=\"index.php\">Go Home</a>
+            or
+            <br>
+            <br>
+</div>";
+}
+else {
+        
 }
