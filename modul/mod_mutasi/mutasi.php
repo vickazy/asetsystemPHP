369c2945731cad21715008cdc1062af01f6e7b6b
herry88 <herry081288@gmail.com>
2017-11-07 14:47:36 +0700

    NewAgain

--- modul/mod_mutasi/mutasi.php
+++ modul/mod_mutasi/mutasi.php
@@ -17,6 +17,7 @@ if(empty($_SESSION['namauser']) AND empty($_SESSION['passuser'])){
 else {
         $aksi = "modul/mod_mutasi/aksi_mutasi.php";
         $act = isset($_GET['act']) ? $_GET['act'] : '';
+         $tanggal = date('dmY');
 
    switch($act){ 
         default:
@@ -80,129 +81,170 @@ else {
                 </li>
             </ul>
             </div>";
-
+            
             echo "<div class=\"row\">
-                    <div class=\"box <col-md-12>\">
-                    <div class=\"box-inner\">
+            <div class=\"box col-md-12\">
+                <div class=\"box-inner\">
                     <div class=\"box-header well\" data-original-title=\"\">
-                        <h2><i class=\"glyphicon glyphicon-edit\"></i> Sistem Mutasi</h2>
+                        <h2><i class=\"glyphicon glyphicon-edit\"></i> Form Add Asset</h2>
                         <div class=\"box-icon\"> 
                             <a href=\"#\" class=\"btn btn-minimize btn-round btn-default\"><i
                                     class=\"glyphicon glyphicon-chevron-up\"></i></a>
                             <a href=\"#\" class=\"btn btn-close btn-round btn-default\"><i
                                     class=\"glyphicon glyphicon-remove\"></i></a>
                         </div>
-                    </div>";
-
-            echo "<div class=\"box-content\">
-                    <form method=\"POST\" action=\"$aksi?module=mutasi&act=update\">
-                        <div class=\"form-group\">
-                            <label for=\"no aset\">No. Aset</label>
-                            <input type=\"text\" class=\"form-control\" name=\"no_aset\">
+                    </div>
+                    <div class=\"box-content\">
+                        <form role=\"form\" method=\"POST\"  action=\"$aksi?module=mutasi&act=update\">
+                            <div class=\"form-group \">
+                                <label>No. Asset</label>
+                                <input type=\"text\" class=\"form-control\"  name=\"no_aset\" placeholder=\"No. Aset\" required>
+                            </div>
+                            
+                            <div class=\"form-group\">
+                                <label>Nama Asset</label>
+                                <input type=\"text\" class=\"form-control \" name=\"nama_aset\"  placeholder=\"Nama Aset\" required>
+                            </div>
+        
+                <div class=\"row\">
+                <div class=\"box col-md-12\">
+                    <div class=\"box-inner\">
+                        <div class=\"box-header well\" data-original-title=\"\">
+                            <h2><i class=\"glyphicon glyphicon-user\"></i> Data PIC Baru</h2>
                         </div>
-
-                        <div class=\"form-group\">
-                            <label for=\"nama aset\">Nama Aset</label>
-                            <input type=\"text\" class=\"form-control\" name=\"nama_aset\">
+                        <div class=\"box-content\">
+                            <table class=\"table\">
+                                 <div class=\"form-group col-md-6\">
+                        <label class=\"control-label\">Noreg</label>
+                        <input type=\"text\" class=\"form-control\" name=\"noreg\" placeholder=\"Noreg\">
+                    </div>
+                    
+                    <div class=\"form-group col-md-6\">
+                        <label>Nama Karyawan</label>
+                        <input type=\"text\" class=\"form-control\"  name=\"karyawan\" placeholder=\"Nama Karyawan\">
+                    </div>
+        
+                    <div class=\"form-group col-md-6\">
+                                <label class=\"control-label\">Area</label>
+                                <div >
+                                    <select id=\"selectError\" name=\"area\" data-rel=\"chosen\">
+                                    <option value=\"0\">-Pilih Data-</option>
+                                    <option value=\"bali-lombok\">Bali-Lombok</option>
+                                    <option value=\"bandung\">Bandung</option>
+                                    <option value=\"baturaja\">Baturaja</option>
+                                    <option value=\"office ho\">Office HO</option>
+                                    <option value=\"bengkulu\">Bengkulu</option>
+                                    <option value=\"jambi 2\">Jambi 2</option>
+                                    <option value=\"jambi 3\">Jambi 3</option>
+                                    <option value=\"jatim-lampung\">Jatim-Lampung</option>
+                                    <option value=\"jawa barat\">Jawa Barat</option>
+                                    <option value=\"kepulauan riau\">Kepulauan Riau</option>
+                                    <option value=\"lebak bulus\">Lebak Bulus</option>
+                                    <option value=\"maluku\">Maluku</option>
+                                    <option value=\"mamuju-majene\">Mamuju Majene</option>
+                                    <option value=\"manado\">Manado</option>
+                                    <option value=\"mirah delima\">Mirah Delima</option>
+                                    <option value=\"nias\">Nias</option>
+                                    <option value=\"nusa tenggara timur\">Nusa Tenggara Timur/option>
+                                    <option value=\"ruko cempaka mas\">Ruko Cempaka Mas</option>
+                                    <option value=\"office makasar\">Office Makasar</option>
+                                    <option value=\"office palembang\">Office Palembang</option>
+                                    <option value=\"offive medan\">Office Medan</option>
+                                    <option value=\"padang sidempuan\">Padang Sidempuan</option>
+                                    <option value=\"palembang\">Palembang</option>
+                                    <option value=\"papua\">Papua</option>
+                                    <option value=\"prs-palopo,rante pao,soroako\">PRS-Palopo,Rante Pao,Soroako</option>
+                                    <option value=\"pts-palopo,tangkerang,soroako\">PTS-Palopo,Tangkerang,Soroako</option>
+                                    <option value=\"sulawesi barat\">Sulawesi Barat</option>
+                                    <option value=\"sulawesi selatan 1\">Sulawesi Selatan 1</option>
+                                    <option value=\"sulawesi selatan 2 (pbe-pinrang,bantaeng,enrekang)\">sulawesi selatan 2 (PBE-Pinrang,Bantaeng,Enrekang)</option>
+                                    <option value=\"sulawesi tenggara\">sulawesi tenggara</option>
+                                    <option value=\"sulawesi tengah\">Sulawesi Tengah</option>
+                                    <option value=\"sumbagut (medan)\">Sumbagut (Medan)</option>
+                                    <option value=\"surabaya-jatim\">Surabaya-Jatim</option>
+                                </select>
+                                </div>
+                            </div>
+                            
+                            <div class=\" form-group col-md-6\">
+                            <label class=\"control-label\" >Kondisi Aset</label>
+                            <div class=\"controls\">
+                                <select id=\"selectError\" name=\"kondisi\" data-rel=\"chosen\">
+                                <option value=\"0\">-Pilih Data-</option>
+                                <option value=\"Dijual\">Dijual</option>
+                                <option value=\"Hilang\">Hilang</option>
+                                <option value=\"Rusak\">Rusak</option>
+                                <option value=\"Masih Digunakan\">Masih Digunakan</option>
+                                <option value=\"Stock HO\">Stock HO</option>
+                                <option value=\"Stock Makasar\">Stock Makasar</option> 
+                                <option value=\"Stock Medan\">Stock Medan</option>
+                                <option value=\"Stock Palembang\">Stock Palembang</option>
+                            </select>
+                            </div>
                         </div>
-            ";
-            echo "</div>";
-            echo "
-            <div>
-            <div class=\"box <col-md-12>\">
-            <div class=\"box-inner\">
-            <div class=\"box-header well\" data-original-title=\"\">
-                <h2><i class=\"glyphicon glyphicon-edit\"></i> PIC Baru</h2>
-                <div class=\"box-icon\"> 
-                    <a href=\"#\" class=\"btn btn-minimize btn-round btn-default\"><i
-                            class=\"glyphicon glyphicon-chevron-up\"></i></a>
-                    <a href=\"#\" class=\"btn btn-close btn-round btn-default\"><i
-                            class=\"glyphicon glyphicon-remove\"></i></a>
+                             <div class=\" form-group col-md-12\">
+                                <label class=\"control-label\" name=\"customer\">Customer</label>
+                                <div class=\"controls\">
+                                    <select id=\"selectError\" name=\"customer\" data-rel=\"chosen\">
+                                    <option value=\"0\">-Pilih Data-</option>
+                                    <option value=\"no customer\">no customer</option>
+                                    <option value=\"tower bersama group\">Tower Bersama Group</option>
+                                    <option value=\"telkomsel\">Telkomsel</option>
+                                    <option value=\"daya mitra telekomunikasi\">Daya Mitra Telekomunikasi</option>
+                                    <option value=\"telkom infra\">Telkom Infra</option>
+                                    <option value=\"inti bangun sejahtera\">Inti Bangun Sejahtera</option> 
+                                    <option value=\"menara seluler nusantara\">Menara Seluler Nusantara</option>
+                                    <option value=\"bintang baja sinar cemerlang\">Bintang Baja Sinar Cemerlang</option>
+                                </select>
+                                </div>
+                            </div>
+                    <br><br>
+        
+                    <div class=\"form-group col-md-6\">
+                        <label class=\"control-label\">Koordinator</label>
+                        <input type=\"text\" class=\"form-control\" name=\"koordinator\" placeholder=\"Koordinator\" id=\"koordinator\">
+                    </div>
+        
+                     <div class=\"form-group col-md-6\">
+                        <label class=\"control-label\">Departemen</label>
+                        <input type=\"text\" class=\"form-control\" name=\"departemen\" placeholder=\"Departemen\">
+                    </div>
+        
+                    <div class=\"form-group col-md-6\">
+                        <label class=\"control-label\">Cluster</label>
+                        <input type=\"text\" class=\"form-control\" name=\"cluster\" placeholder=\"Cluster\">
+                    </div>
+        
+                    <div class=\"form-group col-md-3\">
+                        <label class=\"control-label\">Tgl. Terima</label>
+                        <input type=\"text\" class=\"form-control\" name=\"tgl_terima\"  id=\"datepicker\" value=\"$tanggal\">
+                    </div>  
+        
+                    <div class=\"form-group col-md-12\">
+                        <label>Keterangan</label>
+                        <textarea cols=\"10\" rows=\"15\" name=\"keterangan\" class=\"form-control\"></textarea>
+                    </div>
+                    
+                            </table>
+        
+                        </div>
+                    </div>
                 </div>
-            </div>";
-
-            echo "<div class=\"box-content\">
-                        <div class=\"form-group\">
-                            <label for=\"noreg\">Noreg</label>
-                            <input type=\"text\" class=\"form-control\" name=\"noreg\">
-                        </div>
-                        
-                        <div class=\"form-group\">
-                            <label for=\"nama_karyawan\">Nama Karyawan</label>
-                            <input type=\"text\" class=\"form-control\" name=\"nama_karyawan\">
-                        </div>
-                        
-                        <div class=\"form-group\">
-                        <label class=\"control-label\">Area</label>
-                        <div class='controls'>
-                            <select id=\"selectError\" data-rel=\"chosen\" name=\"area\">
-                            <option value=\"0\">-Pilih Data-</option>
-                            <option value=\"bali-lombok\">Bali-Lombok</option>
-                            <option value=\"bandung\">Bandung</option>
-                            <option value=\"baturaja\">Baturaja</option>
-                            <option value=\"office ho\">Office HO</option>
-                            <option value=\"bengkulu\">Bengkulu</option>
-                            <option value=\"jambi 2\">Jambi 2</option>
-                            <option value=\"jambi 3\">Jambi 3</option>
-                            <option value=\"jatim-lampung\">Jatim-Lampung</option>
-                            <option value=\"jawa barat\">Jawa Barat</option>
-                            <option value=\"kepulauan riau\">Kepulauan Riau</option>
-                            <option value=\"lebak bulus\">Lebak Bulus</option>
-                            <option value=\"maluku\">Maluku</option>
-                            <option value=\"mamuju-majene\">Mamuju Majene</option>
-                            <option value=\"manado\">Manado</option>
-                            <option value=\"mirah delima\">Mirah Delima</option>
-                            <option value=\"nias\">Nias</option>
-                            <option value=\"nusa tenggara timur\">Nusa Tenggara Timur/option>
-                            <option value=\"ruko cempaka mas\">Ruko Cempaka Mas</option>
-                            <option value=\"office makasar\">Office Makasar</option>
-                            <option value=\"office palembang\">Office Palembang</option>
-                            <option value=\"offive medan\">Office Medan</option>
-                            <option value=\"padang sidempuan\">Padang Sidempuan</option>
-                            <option value=\"palembang\">Palembang</option>
-                            <option value=\"papua\">Papua</option>
-                            <option value=\"prs-palopo,rante pao,soroako\">PRS-Palopo,Rante Pao,Soroako</option>
-                            <option value=\"pts-palopo,tangkerang,soroako\">PTS-Palopo,Tangkerang,Soroako</option>
-                            <option value=\"sulawesi barat\">Sulawesi Barat</option>
-                            <option value=\"sulawesi selatan 1\">Sulawesi Selatan 1</option>
-                            <option value=\"sulawesi selatan 2 (pbe-pinrang,bantaeng,enrekang)\">sulawesi selatan 2 (PBE-Pinrang,Bantaeng,Enrekang)</option>
-                            <option value=\"sulawesi tenggara\">sulawesi tenggara</option>
-                            <option value=\"sulawesi tengah\">Sulawesi Tengah</option>
-                            <option value=\"sumbagut (medan)\">Sumbagut (Medan)</option>
-                            <option value=\"surabaya-jatim\">Surabaya-Jatim</option>
-                        </select>
-                        </div>  
-                        </div>
-
-                        <div class=\" form-group\">
-                        <label class=\"control-label\" name=\"customer\">Customer</label>
-                        <div class=\"controls\">
-                            <select id=\"selectError\"  data-rel=\"chosen\" name=\"customer\">
-                            <option value=\"0\">-Pilih Data-</option>
-                            <option value=\"no customer\">no customer</option>
-                            <option value=\"tower bersama group\">Tower Bersama Group</option>
-                            <option value=\"telkomsel\">Telkomsel</option>
-                            <option value=\"daya mitra telekomunikasi\">Daya Mitra Telekomunikasi</option>
-                            <option value=\"telkom infra\">Telkom Infra</option>
-                            <option value=\"inti bangun sejahtera\">Inti Bangun Sejahtera</option> 
-                            <option value=\"menara seluler nusantara\">Menara Seluler Nusantara</option>
-                            <option value=\"bintang baja sinar cemerlang\">Bintang Baja Sinar Cemerlang</option>
-                        </select>
-                        </div><br><br>
-                        <div class=\"form-group\">
-                            <label>Cluster Baru</label>
-                            <input type=\"text\" name=\"cluster\" class=\"form-control\">
-                        </div>
-
-                        <div class=\"form-group\">
-                            <label>Departemen Baru</label>
-                            <input type=\"text\" name=\"departemen\" class=\"form-control\">
-                        </div>
-                        ";        
-
-            echo "</form></div>
-            </div>";
-
+                <!--/span-->
+        </div><!--/row--> 
+             
+             
+                    <button type=\"submit\" class=\"btn btn-default\">Save</button> | 
+                    <button type=\"button\" class=\"btn btn-warning\" onclick=\"self.history.back()\">Cancel</button>
+              
+                </form>
+          
+                    </div>
+                </div>
+            </div>
+            <!--/span-->
+        
+        </div><!--/row-->";
         break;
    }     
 }
