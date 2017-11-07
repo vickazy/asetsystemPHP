4222dd5d00d6d4416183b190b611d8334b47ff2c
herry88 <herry081288@gmail.com>
2017-11-07 17:25:52 +0700

    NewMUtasi

--- modul/mod_mutasi/aksi_mutasi.php
+++ modul/mod_mutasi/aksi_mutasi.php
@@ -1,28 +1,40 @@
 <?php 
+
+function error_404(){
+        ?>
+        <link href=../../css/style_login.css type=text/css rel=stylesheet>
+        <p class=error-code> 404</p>
+                <p class=not-found>Not<br/>Found</p>
+                <div class=clear></div>
+                <div class=content>
+                Silahkan untuk login terlebih dahulu.
+                <br>
+                <a href=index.php>Go Home</a>
+                or
+                <br>
+                <br>
+        </div>
+
+<?php
+}
+
 if(empty($_SESSION['namauser']) AND empty($_SESSION['passuser'])){
-    echo "<link href=\"../css/style_login.css\" type=\"text/css\" rel=\"stylesheet\">
-    <p class=\"error-code\"> 404</p>
-        <p class=\"not-found\">Not<br/>Found</p>
-        <div class=\"clear\"></div>
-        <div class=\"content\">
-            Silahkan untuk login terlebih dahulu.
-            <br>
-            <a href=\"index.php\">Go Home</a>
-            or
-            <br>
-            <br>
-</div>";
+    echo error_404();
 }
 else{
-        //action 
+        include "../../config/koneksi.php";
+        include "../../config/function.php";
+        //action         
         $module = $_GET['module'];
         $act = $_GET['act'];
 
         if($module=='mutasi' AND $act=='update'){
+                $id = $_POST['id'];
                 $no_aset = $_POST['no_aset'];
                 $nama_aset = $_POST['nama_aset'];
                 $noreg = $_POST['noreg'];
-                $pic = $_POST['pic'];
+                $pic = $_POST['karyawan'];
+                $customer = $_POST['customer'];
                 $koordinator = $_POST['koordinator'];
                 $area = $_POST['area'];
                 $tgl_terima = $_POST['tgl_terima'];
@@ -30,6 +42,18 @@ else{
                 $tgl_pindah = $_POST['tgl_pindah'];
                 $keterangan = $_POST['keterangan'];
 
+        $query = "UPDATE input_aset SET no_aset   = '%s',
+                                        nama_aset = '%s',
+                                        noreg     = '%s', 
+                                        area      = '%s', 
+                                        customer  = '%s',
+                                        pic       = '%s' 
+                     WHERE id_input = %d";
 
+        $run_query = sprintf($query, $no_aset,
+                        $nama_aset, $noreg, 
+                        $area, $customer,
+                        $pic,$id);
+        mysqli_query($konek,$run_query);
         }
 }
