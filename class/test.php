2e58fe74b512745cd0280119e44328304e74932a
herry88 <herry081288@gmail.com>
2017-11-08 08:58:41 +0700

    BelajarOOP

---
+++ class/test.php
@@ -0,0 +1,13 @@
+<?php
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    include_once("mutasi.class.php");
+
+    $mutasi = new mutasi();
+
+    $mutasi->setIdMutasi(111);
+    $mutasi->setNoreg("444");
+    $mutasi->setPic("Joe");
+
+    $mutasi->insert();
+?>
