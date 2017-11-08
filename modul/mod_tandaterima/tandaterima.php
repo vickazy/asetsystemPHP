abf3f8cf04d1f3932772b1b1138643e90e3a8da9
herry88 <herry081288@gmail.com>
2017-11-08 16:16:09 +0700

    AdaUpdateNEh

--- modul/mod_tandaterima/tandaterima.php
+++ modul/mod_tandaterima/tandaterima.php
@@ -140,11 +140,44 @@ else {
             </div>
             <!--/span-->
             
-        <div class=\"row\">
-                
-        </div>
-
-
-
+       
         </div><!--/row-->";
+
+        //table
+        echo " <div class=\"row\">
+        <div class=\"box col-md-12\">
+            <div class=\"box-inner\">
+                <div class=\"box-header well\" data-original-title=\"\">
+                    <h2>Daftar Barang</h2>
+
+                    <div class=\"box-icon\">
+                        <a href=\"#\" class=\"btn btn-setting btn-round btn-default\"><i
+                                class=\"glyphicon glyphicon-cog\"></i></a>
+                        <a href=\"#\" class=\"btn btn-minimize btn-round btn-default\"><i
+                                class=\"glyphicon glyphicon-chevron-up\"></i></a>
+                        <a href=\"#\" class=\"btn btn-close btn-round btn-default\"><i
+                                class=\"glyphicon glyphicon-remove\"></i></a>
+                    </div>
+                </div>
+                <div class=\"box-content\">
+                    <table class=\"table table-bordered table-striped table-condensed\">
+                        <thead>
+                        <tr>
+                            <th>Username</th>
+                            <th>Date registered</th>
+                            <th>Role</th>
+                            <th>Status</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                     
+                      
+                       
+                      
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div><!--/span-->";
    }     
